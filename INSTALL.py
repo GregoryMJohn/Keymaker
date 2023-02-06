e5e6604e97ae7cef63b167cfbdf7505dbd80c0e2
Gregory M. John <70587368+GregoryMJohn@users.noreply.github.com>
2023-02-06 09:28:10 -0500

    Add files via upload

---
+++ INSTALL.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+for pkg in ['alive_progress','str2bool']:
+	if not pkg in installed_packages:
+		os.system(f'yes | python3 -m pip install {pkg}')
+
+os.system("git clone https://github.com/nightblade9/simple-english-dictionary.git .")
+os.system("mv ./simple-english-dictionary/data/ .") 
+os.system("yes | rm -R simple-english-dictionary/")
