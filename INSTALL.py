d0350b73db5627fc763ca2d868e2817e430bbf32
Gregory M. John <70587368+GregoryMJohn@users.noreply.github.com>
2023-02-06 10:58:53 -0500

    Update INSTALL.py

--- INSTALL.py
+++ INSTALL.py
@@ -8,15 +8,21 @@ reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
 for pkg in ['alive_progress','str2bool']:
-	if not pkg in installed_packages:
-		cmd = subprocess.Popen('yes', stdout=subprocess.PIPE)
-		subprocess.run(f'python3 -m pip install {pkg}',stdin=cmd.stdout)
-		cmd.wait()
+        if not pkg in installed_packages:
+                subprocess.run(f'yes | python3 -m pip install {pkg}', shell=True)
 
-subprocess.run("git clone https://github.com/nightblade9/simple-english-dictionary.git")
-subprocess.run("mv ./simple-english-dictionary/data/ .") 
-subprocess.run("sudo rm -R simple-english-dictionary/")
-subprocess.run('chmod 755 *.py')
-tee = subprocess.Popen('pwd',stdout=subprocess.PIPE)
-subprocess.run('sudo tee -a ~/.bashrc', stdin=tee.stdout)
-tee.wait()
+os.system("git clone https://github.com/nightblade9/simple-english-dictionary.git")
+os.system("mv ./simple-english-dictionary/data/ .") 
+os.system("sudo rm -R simple-english-dictionary/")
+os.system('chmod 755 *.py')
+cwd = os.getcwd()
+subprocess.run('echo "#Keymaker" | sudo tee -a ~/.bashrc', shell=True)
+subprocess.run(f'echo \'export PATH="{cwd}:$PATH"\' | sudo tee -a ~/.bashrc', shell=True)
+os.system("touch tmp_script.sh")
+os.system("chmod 777 tmp_script.sh")
+with open("tmp_script.sh","w") as fh:
+        fh.write("#!/bin/bash \n")
+        fh.write("source ~/.bashrc")
+
+subprocess.run("./tmp_script.sh")
+os.remove("tmp_script.sh")
