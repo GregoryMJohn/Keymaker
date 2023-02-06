1eaef35a9cb13e4516e0fed9c4fd12b743ace7ee
Gregory M. John <70587368+GregoryMJohn@users.noreply.github.com>
2023-02-06 09:29:35 -0500

    Update INSTALL.py

--- INSTALL.py
+++ INSTALL.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import subprocess
 
 reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
